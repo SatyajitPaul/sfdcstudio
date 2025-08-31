5acbccc7d9ffeb317eee5b674949f3e9473f79ae
Satyajit Paul <paulsjit@gmail.com>
2025-08-31 16:26:40 +0530

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: {
@@ -68,6 +69,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-8VNJCHMP8X" />
     </html>
   );
 }
